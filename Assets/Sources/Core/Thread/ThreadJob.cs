31ebcceabeb7e93768b5e505f4a32d7a21b42e4c
木宛城主 <1194679215@qq.com>
2017-05-23 18:51:27 +0800

    添加对异步Thread的支持

---
+++ Assets/Sources/Core/Thread/ThreadJob.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class ThreadJob 
+{
+	private volatile bool _isDown;
+	private Thread _thread;
+
+	private Action _onBeginHandler;
+	private Action _onFinishHandler;
+
+	public ThreadJob(Action onBeginHandler,Action onFinishedHandler)
+	{
+		this._onBeginHandler = onBeginHandler;
+		this._onFinishHandler = onFinishedHandler;
+	}
+
+	public virtual void Start(){
+		_thread = new Thread (Run);
+		_thread.Start ();
+	}
+
+	public virtual void Abort(){
+		_thread.Abort ();
+	}
+
+	public void ThreadHandler(){
+		if(this._onBeginHandler!=null){
+			this._onBeginHandler ();
+		}
+	}
+
+	public void OnFinished(){
+		if(this._onFinishHandler!=null){
+			this._onFinishHandler ();
+		}
+	}
+
+	public virtual bool Update(){
+		if(_isDown){
+			OnFinished ();
+			return true;
+
+		}
+		return false;
+	}
+	public IEnumerator WaitFor(){
+		while(!Update()){
+			//暂停协同程序，下一帧再继续往下执行
+			yield return null;
+		}
+	}
+	private void Run(){
+		ThreadHandler ();
+		_isDown = true;
+	}
+}
---
+++ Assets/Sources/Core/Thread/ThreadJobTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+namespace AssemblyCSharp
+{
+	public class ThreadJobTest:MonoBehaviour
+	{
+		
+		ThreadJob job;
+		bool stop;
+		void Start(){
+
+//			job= new ThreadJob (
+//				() => {
+//					//heavy job
+//					for(int i=0;i<12345;i++){
+//
+//						Debug.Log(i);
+//					}
+//				},
+//			
+//				() => {
+//					Debug.Log("结束了");
+//				});
+//			job.Start ();
+
+			for(int i=0;i<12345;i++){
+
+				Debug.Log(i);
+			}
+
+			StartCoroutine (Wait());
+		}
+
+		IEnumerator Wait(){
+
+			yield return StartCoroutine (job.WaitFor());
+
+			stop = true;
+
+		}
+
+		void Update()
+		{
+			
+			if (!stop) {
+				// rotate cube to see if main thread has been blocked;
+				transform.Rotate(Vector3.up, Time.deltaTime * 180);
+			}
+		}
+	}
+}
+
