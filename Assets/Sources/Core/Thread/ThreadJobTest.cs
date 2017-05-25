0929c2ef82a696fb595c2238833203fa4aa2e75a
木宛城主 <1194679215@qq.com>
2017-05-25 23:08:57 +0800

    Thread与Coroutine联用

--- Assets/Sources/Core/Thread/ThreadJobTest.cs
+++ Assets/Sources/Core/Thread/ThreadJobTest.cs
@@ -1,52 +1,55 @@
 ﻿using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace AssemblyCSharp
 {
 	public class ThreadJobTest:MonoBehaviour
 	{
-		
-		ThreadJob job;
-		bool stop;
+
+	    public Image greenRect;
+	    public Image pinkRect;
+
+        ThreadJob job;
+
+	    void Awake()
+	    {
+            job = new ThreadJob(
+            () => {
+                //heavy job
+                for (int i = 0; i < 12345; i++)
+                {
+
+                }
+            },
+
+            () => {
+                Debug.Log("结束了");
+            });
+        }
+	
 		void Start(){
 
-			job= new ThreadJob (
-				() => {
-					//heavy job
-					for(int i=0;i<12345;i++){
-
-						Debug.Log(i);
-					}
-				},
-			
-				() => {
-					Debug.Log("结束了");
-				});
-			job.Start ();
-
-//			for(int i=0;i<12345;i++){
-//
-//				Debug.Log(i);
-//			}
-//
-			StartCoroutine (Wait());
+			StartCoroutine (Go());
 		}
 
-		IEnumerator Wait(){
+		IEnumerator Go()
+		{
 
-			yield return StartCoroutine (job.WaitFor());
+		    pinkRect.transform.DOLocalMoveX(250, 1.0f);
+            yield return new WaitForSeconds(1);
+		    pinkRect.transform.DOLocalMoveY(-150, 2);
+            yield return new WaitForSeconds(2);
 
-			stop = true;
+            //yield return StartCoroutine (job.WaitFor());
 
 		}
 
 		void Update()
 		{
-			
-			if (!stop) {
-				// rotate cube to see if main thread has been blocked;
-				transform.Rotate(Vector3.up, Time.deltaTime * 180);
-			}
+           greenRect.transform.Rotate(Vector3.left, Time.deltaTime * 180);
 		}
 	}
 }
