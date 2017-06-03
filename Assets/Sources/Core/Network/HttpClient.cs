0bd506ce5b54e0d578de874ac9b28d296ba000af
木宛城主 <1194679215@qq.com>
2017-06-03 23:58:38 +0800

    UnityWebRequest体验

--- Assets/Sources/Core/Network/HttpClient.cs
+++ Assets/Sources/Core/Network/HttpClient.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Sources.Core.HTTP;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Assets.Sources.Core.Network
 {
@@ -13,9 +17,31 @@ namespace Assets.Sources.Core.Network
             
         }
 
-        public void Get()
+        public void Get(Action<string> action )
         {
-            
+            HttpTool.Instance.StartCoroutine(WaitUntilResponseReceived(action));
+        }
+
+        private IEnumerator WaitUntilResponseReceived(Action<string> action)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get("http://www.cnblogs.com"))
+            {
+                yield return www.Send();
+
+                if (www.isError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    // Show results as text
+                    //Debug.Log(www.downloadHandler.text);
+
+                    action(www.downloadHandler.text);
+                    // Or retrieve results as binary data
+                    byte[] results = www.downloadHandler.data;
+                }
+            }
         }
     }
 }
---
+++ Assets/Sources/Views/NetworkView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Sources.Core.Network;
+using uMVVM.Sources.Infrastructure;
+using UnityEngine;
+
+namespace Assets.Sources.Views
+{
+    public class NetworkView:MonoBehaviour
+    {
+        void Start()
+        {
+            HttpClient.Instance.Get((value) =>
+            {
+                Debug.Log(value);
+            });
+        }
+    }
+}
