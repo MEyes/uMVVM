37e4aa60b98f60bcadf35773e68e65a0e58596b8
木宛城主 <1194679215@qq.com>
2017-06-07 21:22:25 +0800

    添加动画，检查网络请求不阻塞主线程

--- Assets/Sources/Views/NetworkView.cs
+++ Assets/Sources/Views/NetworkView.cs
@@ -14,18 +14,25 @@ namespace Assets.Sources.Views
 {
     public class NetworkView:MonoBehaviour
     {
-        public Image image;
+        public Image greenRect;
+
+        private bool stop;
         void Start()
         {
             var repository=new RemoteRepository<Identity>();
             //Loding
 			repository.Get<Response<User>> ("http://192.168.199.233/User/List", new Identity (), (response) => {
 				Debug.Log(response.Items[0].Address.Name);
+			    stop = true;
 			});
-				
-//			repository.Post ("http://192.168.199.233/User/Post", new Identity (){Token="abc",DeviceId="def"}, (response) => {
-//				Debug.Log(response.Name);
-//			});
+        }
+
+        void Update()
+        {
+            if (!stop)
+            {
+                greenRect.transform.Rotate(Vector3.left, Time.deltaTime * 180);
+            }
         }
     }
 }
